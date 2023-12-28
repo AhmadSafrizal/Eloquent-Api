c5a9acec68f7b9ebb0bce70b8ff9b77ddfdd076d
unknown <ahmadsafrizal01@gmail.com>
2023-12-28 14:04:20 +0700

    Eloquent API

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\ProductSeeder;
+use App\Models\Product;
+
+class ProductTest extends TestCase
+{
+    public function testProduct()
+    {
+        $this->seed([
+            CategorySeeder::class,
+            ProductSeeder::class,
+        ]);
+
+        $product = Product::first();
+
+        $this->get("/api/products/$product->id")
+            ->assertStatus(200)
+            ->assertJson([
+                "value" => [
+                    "name" => $product->name,
+                    "category" => [
+                        "id" => $product->category->id,
+                        "name" => $product->category->name
+                    ],
+                    "price" => $product->price,
+                    "is_expensive" => $product->price > 1000,
+                    "created_at" => $product->created_at->toJSON(),
+                    "updated_at" => $product->updated_at->toJSON(),
+                ]
+            ]);
+    }
+
+    public function testCollectionWrap()
+    {
+        $this->seed([
+            CategorySeeder::class,
+            ProductSeeder::class,
+        ]);
+
+        $response = $this->get("/api/products")
+            ->assertStatus(200)
+            ->assertHeader("X-Powered-BY", "Asaf");
+        
+        $names = $response->json("data.*.name");
+        
+        for ($i = 0; $i < 5; $i++) {
+            self::assertContains("Product $i of Food", $names);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            self::assertContains("Product $i of Drink", $names);
+        }
+    }
+
+    public function testProductPaging()
+    {
+        $this->seed([
+            CategorySeeder::class,
+            ProductSeeder::class,
+        ]);
+
+        $response = $this->get("/api/products-paging")
+            ->assertStatus(200);
+        
+        self::assertNotNull($response->json("links"));
+        self::assertNotNull($response->json("meta"));
+        self::assertNotNull($response->json("data"));
+    }
+
+    public function testAdditional()
+    {
+        $this->seed([
+            CategorySeeder::class,
+            ProductSeeder::class,
+        ]);
+
+        $product = Product::first();
+
+        $response = $this->get('/api/products-debug/' . $product->id)
+            ->assertStatus(200)
+            ->assertJson([
+                "author" => "Asaf",
+                "data" => [
+                    "id" => $product->id,
+                    "name" => $product->name,
+                    "price" => $product->price,
+                ]
+            ]); 
+        
+        self::assertNotNull($response->json("server_time"));
+    }
+}
